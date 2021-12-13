225e7e1e20723ff1b3152f16b8f28cbf5d21ab3d
Julia-upc <juliasanchezmartinez98@gmail.com>
2021-12-13 11:51:09 +0100

    tutorial

---
+++ Shiny_tutorial/App-3/app.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+               information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = list("Percent White", 
+                                 "Percent Black",
+                                 "Percent Hispanic", 
+                                 "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+    
+    mainPanel(
+      h1("Census Vis"),
+      p("This is how it will look like")
+    )
+  )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
